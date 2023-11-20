dc8c29cbbb3a9e13f74e3a20e940c0fea7f6eb46
AlessandroCampo <alessandrocampo97@gmail.com>
2023-11-20 18:05:28 +0100

    fixed flag bug

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const list = document.getElementById("google_translate_element")
 list.addEventListener("input", checkFlag)
 function checkFlag() {
     console.log("ciao")
+    console.log(event.target.value)
     let flag = document.getElementById("flag")
-    flag.src = "img/" + list.value + ".png"
+    let langRef = document.getElementById("lang-ref")
+    flag.src = "img/" + event.target.value + ".png"
 }
 
 
